5e1025544a41c5569a59d27a9c0cfdca40934668
nunesotavio <89364019+nunesotavio@users.noreply.github.com>
2021-08-22 21:36:38 -0300

    Update RunPSCF.py

--- RunPSCF.py
+++ RunPSCF.py
@@ -1,9 +1,9 @@
-'''===========================================================================================================
+'''===============================================================================================================
 Federal University of Santa Catarina
 Department of Sanitary and Environmental Engineering
 Authors: Otávio Nunes dos Santos, Leonardo Hoinaski
 Incorporating gridded concentration data in air pollution back trajectories analysis for source identification
-===========================================================================================================''' 
+==================================================================================================================''' 
 
 #%% ---------------------- Import of modules
 from webScrapping import Hysplit_Scrap
